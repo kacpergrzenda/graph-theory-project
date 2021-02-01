44b74c2028167722b00d97cf07988040c892744b
kacpergrzenda <G00373427@gmit.ie>
2021-02-01 20:13:33 +0000

    Ifs and Whiles Practice

--- basics.py
+++ basics.py
@@ -1,48 +1,29 @@
-# Ian Mcloughlin 
+# Kacper Grzenda
 # Python basics
 # 2021-01-30
 
-print("Hello, World!")
+#Function
+def facotrial(n):
+    """ Number to calculate factorial of."""
+    #Deal with negative inputs
+    if n < 1:
+        m = -n
+    else:
+        m = n
+    
+    #the running total - eventually the factorial
+    total = 1
+    #Loop to do the multiplications
+    while m > 0:
+        total = total * m
+        m = m - 1
+    # Return the answer.
+    if n < 1:
+        return -total
+    else:
+        return total
 
-a = 1
-b = 1.0
-s = "Hello, world from  a String!"
-t = '"Hello", from a diffrent String!'
-
-print(a, b, s, t)#Print the four variables to screen
-
-print(type(b))#What type is b
-
-print(s[0]) #Whats the first character of string s
-
-print(s[0:2])#Print out substring
-
-print(s[3:10:2])#For loop to search string ( 3 is your i, 3 <10 , interate by 2)
-
-x = [1, 2, 3, "Hello"]
-print(x)
-print(x[0])
-print(x[2])
-print(x[-1])
-
-#Loop through x array 
-#for i in x[::2] give me every second element of x
-for i in x:
-    print(i)
-    print(i + i)
-
-for i in range(10):
-    print(i)
-
-d = {"no_wheels": 4, "make": "Skoda"}#Dictionary
-#list comprehensions
-d["model"] = "Superb" #Giving a dictionary element a key
-print(d["no_wheels"])
-
-r = [1,2,3,4]#list
-
-print(r)
-
-s = [i*i for i in r]#lit made using another list 
-
-print(s)
+#test the function
+n = -20
+# Calculate the factorial of n.
+print(f"The factorial of {n} is {facotrial(n)}.")
