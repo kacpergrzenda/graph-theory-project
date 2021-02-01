10f7d58f64b1b9d4e185cc4106f459a1b7785da3
kacpergrzenda <G00373427@gmit.ie>
2021-02-01 19:02:24 +0000

    Looked at the basics

---
+++ basics.py
@@ -0,0 +1,20 @@
+# Ian Mcloughlin 
+# Python basics
+# 2021-01-30
+
+print("Hello, World!")
+
+a = 1
+b = 1.0
+s = "Hello, world from  a String!"
+t = '"Hello", from a diffrent String!'
+
+print(a, b, s, t)#Print the four variables to screen
+
+print(type(b))#What type is b
+
+print(s[0]) #Whats the first character of string s
+
+print(s[0:2])#Print out substring
+
+print(s[3:10:2])#For loop to search string
