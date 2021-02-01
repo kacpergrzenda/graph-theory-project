99b84e94fe43a52046d2366247dad005c5623b99
kacpergrzenda <G00373427@gmit.ie>
2021-02-01 19:45:41 +0000

    Lists and dictionaries

--- basics.py
+++ basics.py
@@ -17,4 +17,32 @@ print(s[0]) #Whats the first character of string s
 
 print(s[0:2])#Print out substring
 
-print(s[3:10:2])#For loop to search string
+print(s[3:10:2])#For loop to search string ( 3 is your i, 3 <10 , interate by 2)
+
+x = [1, 2, 3, "Hello"]
+print(x)
+print(x[0])
+print(x[2])
+print(x[-1])
+
+#Loop through x array 
+#for i in x[::2] give me every second element of x
+for i in x:
+    print(i)
+    print(i + i)
+
+for i in range(10):
+    print(i)
+
+d = {"no_wheels": 4, "make": "Skoda"}#Dictionary
+#list comprehensions
+d["model"] = "Superb" #Giving a dictionary element a key
+print(d["no_wheels"])
+
+r = [1,2,3,4]#list
+
+print(r)
+
+s = [i*i for i in r]#lit made using another list 
+
+print(s)
