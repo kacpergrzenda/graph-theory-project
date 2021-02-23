03602e9999973977a7abf2d7933b879611fb8b2f
kacpergrzenda <G00373427@gmit.ie>
2021-02-23 18:57:55 +0000

    Automaton and formal language

---
+++ formalLanguages.py
@@ -0,0 +1,17 @@
+"""automata """
+# Kacper Grzenda
+# 2021-02-23
+
+#Order doesnt exist in Sets
+#a = {0,1} #characters '0' '1'
+#a^0  = {} # ""
+#a^1 = {0,1} #Strings "0" "1"
+#a^2 = {00, 01, 10, 11}
+#a^3 = {000, 001, 010, 011, 100, 101, 110, 111}   >>> (0,0,1) >>> {1,{0,{0,{}}}} #list of characters
+
+#a* = "UiA^i" # i element of natrual numbers
+#a* = {"E", 0, 1, 00, 01, 10, 11, 000, 001, 010, 100, 101, 110, 111} 
+
+#L = {"E", 0, 00, 11, 000, 011, 101, ....}
+
+#L C_ A*
